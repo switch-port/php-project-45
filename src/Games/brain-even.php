a6102e12b3b89077d5ef748c513562baed786c5d
deduhm <shim.master3490@gmail.com>
2023-08-18 22:07:28 +0300

    Refactored brain-calc

---
+++ src/Games/brain-even.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace BrainGames\Games\Even;
+require_once __DIR__ . '/../../vendor/autoload.php';
+
+use BrainGames\Engine\Engine;
+
+function startGameBrainEven(): void
+{
+    $countCorrectAnswers = 0;
+    $winCode = true;
+
+    $engine = new Engine('Answer "yes" if the number is even, otherwise answer "no".');
+
+    while ($countCorrectAnswers < 3) {
+        try {
+            $randomNumber = random_int(Engine::MIN_INT, Engine::MAX_INT);
+        } catch (\Exception $e) {
+        }
+
+        $engine->printQuestion((string)$randomNumber);
+
+        if (isEven($randomNumber)) {
+            $correctAnswer = "yes";
+        } else {
+            $correctAnswer = "no";
+        }
+
+        $engine->setUserAnswer();
+
+        if ($engine->getUserAnswer() == $correctAnswer) {
+            $engine->printCorrect();
+            $countCorrectAnswers++;
+        } else {
+            $engine->gameOver($correctAnswer);
+            $winCode = false;
+            break;
+        }
+    }
+
+    if ($winCode) {
+        $engine->winGame();
+    }
+}
+
+function isEven(int $number): bool
+{
+    if ($number%2 == 0) {
+        return true;
+    } else {
+        return false;
+    }
+}
