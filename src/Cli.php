967ed61300be1c32d4a9e1050f16a197c9e4b363
deduhm <shim.master3490@gmail.com>
2023-08-18 15:55:26 +0300

    added Cli.php changed bin file brain-games

---
+++ src/Cli.php
@@ -0,0 +1,15 @@
+<?php
+namespace BrainGames\Cli;
+
+use function cli\line;
+use function cli\prompt;
+
+function printLine(string $str): void
+{
+    line($str);
+}
+
+function getPrompt(string $str): string
+{
+    return prompt($str);
+}
